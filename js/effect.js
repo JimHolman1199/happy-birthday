0c1301acd9a4e2997d6dbd7c51078327d167c257
jimholman1199 <jimholman1199@gmail.com>
2020-05-04 17:16:55 +0300

    [Magic]

--- js/effect.js
+++ js/effect.js
@@ -7,7 +7,7 @@ $("document").ready(function() {
   var vw;
   $(window).resize(function() {
     vw = $(window).width() / 2;
-    $("#b1,#b2,#b3,#b4,#b5").stop();
+    $("#b1,#b2,#b3,#b4,#b5,#b6").stop();
     $("#b11").animate({ top: 240, left: vw - 180 }, 500);
     $("#b22").animate({ top: 240, left: vw - 120 }, 500);
     $("#b33").animate({ top: 240, left: vw - 60 }, 500);
@@ -97,11 +97,18 @@ $("document").ready(function() {
       loopFive();
     });
   }
+  function loopSix() {
+    var randleft = 1000 * Math.random();
+    var randtop = 500 * Math.random();
+    $("#b6").animate({ left: randleft, bottom: randtop }, 10000, function() {
+      loopSix();
+    });
+  }
 
   $("#balloons_flying").click(function() {
     $(".balloon-border").animate({ top: -500 }, 8000);
     $("#b1,#b4,#b5").addClass("balloons-rotate-behaviour-one");
-    $("#b2,#b3").addClass("balloons-rotate-behaviour-two");
+    $("#b2,#b3,#b6").addClass("balloons-rotate-behaviour-two");
     loopOne();
     loopTwo();
     loopThree();
@@ -146,12 +153,14 @@ $("document").ready(function() {
     $("#b3").attr("id", "b33");
     $("#b4").attr("id", "b44");
     $("#b5").attr("id", "b55");
+    $("#b6").attr("id", "b66");
 
     $("#b11").animate({ top: 240, left: vw - 180 }, 500);
     $("#b22").animate({ top: 240, left: vw - 120 }, 500);
     $("#b33").animate({ top: 240, left: vw - 60 }, 500);
     $("#b44").animate({ top: 240, left: vw + 1 }, 500);
     $("#b55").animate({ top: 240, left: vw + 60 }, 500);
+    $("#b66").animate({ top: 240, left: vw + 120 }, 500);
 
     $(".balloons").css("opacity", "0.9");
     $(".balloons h2").fadeIn(3000);
